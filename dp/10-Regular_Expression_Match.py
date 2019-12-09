fdbe66d50648e8353336bd3f57456fb1d41bd1da
wangluolin <www970301@126.com>
2019-12-09 18:20:11 +0800

    add annotation

--- dp/10-Regular_Expression_Match.py
+++ dp/10-Regular_Expression_Match.py
@@ -10,7 +10,7 @@ class Solution:
     def isMatch(self, s: str, p: str) -> bool:
         if not p: 
             return not s
-        first_match = bool(s) and p[0] in {s[0], '.'}
+        first_match = bool(s) and p[0] in {s[0], '.'} # bool(s) means s not null
         if len(p) >1 and p[1] == '*':
             return (self.isMatch(s, p[2:])) or (first_match and self.isMatch(s[1:], p))
         else:
